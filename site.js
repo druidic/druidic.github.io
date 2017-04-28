df7218d571c4f8dabe08e5e66e0ec288a1e52271
Ben Christel <bchristel@pivotal.io>
2017-04-28 08:35:40 -0700

    Edit copy in site.js

--- site.js
+++ site.js
@@ -10,132 +10,55 @@ read on!
 
 === Why?
 
-Our  lives  are increasingly filled with software.
-Much of the software we rely on is overcomplicated and buggy
-because it's not designed with the user's needs first.
-Companies want you to live on the bleeding edge of technology
-so they can sell you more stuff, and to this end
+         "The number of transitors on a chip
+          (and thus, the power of computers)
+          doubles every eighteen months"
+                               --Moore's law
 
-Sometimes this is because companies are competing to have
-the most features in their product, irrespective of what
-their users actually need. Sometimes ad-supported software
-is specifically designed to distract, divert, or addict
-you, because the more time you spend using it the more
-revenue it brings in. Sometimes, products are optimized
-for being pleasant to use in the store, not for actual day-to-day
-use.
+         "The speed of software halves every
+          eighteen months"
+                               --Gates' Law
 
+If you're like us, computers frustrate you.
 
+You know, intellectually, that modern computers are orders
+of magnitude more powerful than the computers of two decades
+ago. And yet, it seems like so little progress has been
+made--computers are still slow, still buggy, still have
+compatibility issues and software that won't install
+properly. You wish things would just WORK. How hard could
+that really be?
 
-1. A lot of computer technology isn't designed to benefit users
-  a. Software bloat
-  b. Moral hazard of advertising
-  c. Overpromises - works under ideal conditions (i.e. in the store)
-2. Users can't opt-out of changes because old software
-   and hardware stop being available.
-3. The Grove lets you opt-in to a world of user-centered, permanent software
-   -- software that stays simple and always works.
+I'm painting with a very broad brush here, but the reason
+we're in this mess is that histor-
+ically, whenever something didn't work quite right, the answer from
+software companies and software developers has been MORE
+SOFTWARE. And who can blame them? That's what they get paid
+for. But over the years, this has created a teetering
+stack of tools, apps, and platforms. 99.9% of the time,
+it all basically works. One time in a thousand, things fail
+in totally unexpected, bizarre, stupid ways, and the only
+way to understand what went wrong is to understand
+EVERYTHING in the stack, because everything is connected
+to everything else.
 
-Our  lives  are increasingly filled with software--software that
-always seems a little bit teetery, like it's just barely hanging
-together.  Bugs,  errors, and confusing interfaces are the norm.
-The bugs eventually get fixed, but it  seems  like  as  soon  as
-they're  ironed  out,  the  state of the art has moved on to the
-next big thing. A new product gets rolled out, full of half-kept
-promises, and the cycle continues.
+Rather than
+live on the bleeding edge of software bloat, we want to
+approach software with a mentality of sufficiency--the idea
+that a limited feature set can adequately serve almost
+everyone's needs. It turns out that if you approach software
+development from this perspective, a lot of the problems
+simply go away. Since we're not straining the hardware to
+its limits with extraneous features, we can eschew
+optimizations in our code that give bugs places to hide.
+Since we don't add extra features, we don't need to force
+people to upgrade their software. Since the hardware
+requirements for our software are minimal, our applications
+run as well on a cheap netbook as on a professional PC.
 
-There are a bunch of reasons why the computing world works  this
-way, but the big one is that it benefits the companies that make
-computers and software. A company that never updates its product
-will soon go out of business as the market saturates. In order
-to stay afloat, companies need to churn out "new and improved"
-versions of their product so their customers will keep paying.
-In theory, the new features are valuable to the people who use
-the software. In reality, they're often dead weight, serving
-only to slow the whole system down and add to an already be-
-wildering myriad of tools and options.
-
-Since more complex software gobbles more memory
-and processor power, you're also forced to have the latest hardware
-to keep your software up to date. Computer manufacturers like
-this, obviously.
-
-There's an extra layer of perniciousness when free software
-and websites are supported by advertisements. Unlike pay-to-download
-apps, the advertising model encourages software designers to try
-to  get  you to stay on the site or in the app as long as poss-
-ible, and come back as often as possible, so you see more  ads.
-This  has  led  to the rise of apps that work, psychologically,
-like slot machines. Their goal is not to serve you, but to  get
-you hooked. In practical terms, that often means making it *harder*
-to achieve what you want to do with the app, directing your
-attention instead toward what advertisers want to you do.
-
-Worse than the upgrade rat-race is the fact that you often have
-no real way to opt out. Websites, operating systems, and apps
-can change overnight without giving us a say in the matter. Given that
-our day-to-day computer use revolves around self-upgrading
-software, it can be easy to feel that we don't truly own or have
-control over our computers, even as we become more and more
-dependent on them.
-
-Druidic's mission is to give you a way out, a path to escape
-the samsaric cycles of software bloat, addictive apps,
-and forced upgrades. The first step on that path is the Grove
-computer--a simulated PC that runs in your web browser.
-Hey look! You're using one right now.
-
-The Grove is the antithesis of modern software design, and
-therein lies its strength. Because it's so primitive,
-it can run basically anywhere, and it's built to last forever.
-Its only requirements are a web browser and
-a keyboard, and both of those artifacts are going to be with us for a long, long time.
-(no, voice and gestural interfaces won't kill the keyboard, in large part because programmers
-will always need super-precise control over the input they give to their computers.)
-I have a hard time imagining what technology will be like in 2050, but
-I'd be willing to bet that you'll still be able to run a Grove. And that
-means that any data you commit to a Grove computer will still be
-accessible, through exactly the same interface that you used
-to create it.
-
-I don't know about you, but I yearn for that kind of permanence.
-Such swathes of our lives are consumed by consuming ephemera--even
-physical things aren't built to be permanent, anymore.
-Can you imagine leaving any of your current possessions to your great-grandkids?
-How about leaving them a *chunk of software*? The thought should
-seem ridiculous. Until now, it *was* ridiculous.
-
-But we're setting out to make it a reality. Heirloom-quality code.
-That's our goal.
-
-Non-programmers may never have thought about the process of
-as an artifact whose creation has a process.
-But just like a piece of furniture, software can be slapped
-together cheaply or carefully handcrafted. It can be sturdy or
-flimsy, ergonomic or uncomfortable, simple or ornamented. It can
-be a throwaway commodity, or something unique and personal that
-brings joy every time you use it. Many people have probably never
-recognized the potential of computers to create that kind of joy.
-But they can. They totally can! And just as with a piece of
-furniture, craftsmanship, ergonomics, and attention to detail
-all contribute to the experience.
-
-I'm not claiming that the Grove is going to replace
-all the technology you currently use. By definition, it can't do that,
-because in order to exist it needs a real computer with a real OS
-and a browser. Plus, you're still going to want to watch YouTube videos
-and check Facebook, and the Grove will never help you with those things.
-It can't, because they're ephemeral, and it would suck if we built
-the world's coolest YouTube-based social app for the Grove, and then
-twenty years later YouTube doesn't exist anymore. Permanence means,
-unfortunately, that you can't depend on things that might go away.
-
-But so many other things *are* possible! The Grove can
-handle programming, word-processing, spreadsheets,
-personal organization, and retro-style games. I also have
-this idea for a multimedia webzine of art, text, and programs that
-would run on the Grove.
-
+Throughout our design and development cycle, we're
+constantly asking ourselves: "do we really need this
+feature?" "how can this be made simpler?"
 
 === Philosophy
 
